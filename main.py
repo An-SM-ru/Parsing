c9348c168976d31e925b0db82f4ee7d7fa051a6e
smirnov <smirnov.andrew.e@gmail.com>
2022-03-11 17:34:46 +0500

    Переделал на функции с выгрузкой в файл

--- main.py
+++ main.py
@@ -1,20 +1,43 @@
 import requests
-from bs4 import BeautifulSoup # Для анализа html/css кода
+import json
+
 
 url_kfc = 'https://api.kfc.com/api/store/v2/store.get_restaurants' # адрес анализа
-
 r = requests.get(url_kfc) # записываем ответ сервера в переменную r
-
 address_kfc = r.json() # Получаем данные о ресторанах
 
-class Rest_KFC():
+count_restaurants = len(address_kfc['searchResults']) # Общее кол-во ресторанов КФС
+data_json = {"count": count_restaurants,
+             "restaurants": []}
 
-    def __init__(self, address_kfc, number):
-        self._number = number
-        self._address_kfc = address_kfc
 
-        self.rest_title = self._address_kfc['searchResults'][self._number]['storePublic']['title']['ru']
-        self.rest_address = self._address_kfc['searchResults'][self._number]['storePublic']['contacts']['streetAddress']['ru']
+def get_rest(address_kfc,number):
+    # Пробегаем по нужным нам полям, получаем значения
+    try:
+        rest_name = address_kfc['searchResults'][number]['storePublic']['title']['ru']
+        rest_address = address_kfc['searchResults'][number]['storePublic']['contacts']['streetAddress']['ru']
+        rest_city = address_kfc['searchResults'][number]['storePublic']['contacts']['city']['ru']
+        rest_coordinates = address_kfc['searchResults'][number]['storePublic']['contacts']['coordinates']['geometry']['coordinates']
+        # Возвращаем словарь с нужными нам полями
+        return {"id": number+1,
+               "name": rest_name,
+               "address": rest_address,
+               "city": rest_city,
+               "coordinates": rest_coordinates}
+    except:
+        # Если значения ошибочные то ничего не возвращаем
+        return False
 
-kfc = Rest_KFC(address_kfc, 10)
-print(f'Название "{kfc.rest_title}" по адресу: {kfc.rest_address}')
+
+def main_rest():
+    # Пробегаем по всем значениям, если не пустые и не вызывают ошибок добавляем в базу
+    for id in range(count_restaurants):
+        if get_rest(address_kfc, id)!=False:
+            data_json['restaurants'].append(get_rest(address_kfc, id))
+    # Сохраняем всю базу в файл
+    with open('d:\data_rest_kfc.txt', 'w') as outfile:
+        json.dump(data_json, outfile, ensure_ascii=False)
+
+
+if __name__ == "__main__":
+	main_rest()
